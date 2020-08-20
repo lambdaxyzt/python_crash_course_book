7bfb78eae4fc5afc51dc7dd9f12d09e02544aa16
Lambdaxyzt <Lambdaxyzt@gmail.com>
2020-08-21 02:57:09 +0430

    add excersise file

---
+++ (2-3_2-6)_Chapter1_Strings_try_it_yourself_page25.py
@@ -0,0 +1,24 @@
+#2-3
+name = "eric"
+massage = f"Hello {name}, would you like to learn some Python today?"
+print(massage)
+
+#2-4
+name = "eric"
+print( name.title() )
+print( name.upper() )
+print( name.lower() )
+
+#2-5
+massage = 'Albert Einstein once said, "A person who never made a mistake never tried anything new."'
+
+#2-6
+famous_person = "Albert Einstein"
+massage = f'{famous_person} once said, "A person who never made a mistake never tried anything new."'
+
+#2-7
+name = "\t python \n"
+print( name.lstrip() )
+print( name.rstrip() )
+print( name.sstrip() )
+
