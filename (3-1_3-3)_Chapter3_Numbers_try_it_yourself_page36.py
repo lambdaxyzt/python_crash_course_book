9c080dc970e5ade286a93f3a5022b7d3a848352e
Lambdaxyzt <Lambdaxyzt@gmail.com>
2020-08-25 20:25:15 +0430

    compelete style and html file and new excercises

---
+++ (3-1_3-3)_Chapter3_Numbers_try_it_yourself_page36.py
@@ -0,0 +1,18 @@
+#3-1
+frind_list = ["john","erick","maria"]
+print(frind_list[0])
+print(frind_list[1])
+print(frind_list[2])
+
+
+#3-2
+print( f"Hello Dear {frind_list[0]}, How are you ?" )
+print( f"Hello Dear {frind_list[1]}, How are you ?" )
+print( f"Hello Dear {frind_list[2]}, How are you ?" )
+
+#3-3
+my_vehicle = ["lamborghini","porsche","maserati","ferrari"]
+print( f"I would like to own {frind_list[0]}" )
+print( f"I would like to own {frind_list[1]}" )
+print( f"I would like to own {frind_list[2]}" )
+print( f"I would like to own {frind_list[3]}" )
